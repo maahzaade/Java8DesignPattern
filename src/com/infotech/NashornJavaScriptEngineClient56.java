103e9620c90c96e82b196915231a8b3004a89201
mahdiye.nikraftar <mahdiye.nikraftar@cgi.com>
2021-02-10 12:14:49 -0500

    Nashorn:Invoke JavaScript function in java 8

--- src/com/infotech/NashornJavaScriptEngineClient56.java
+++ src/com/infotech/NashornJavaScriptEngineClient56.java
@@ -1,5 +1,6 @@
 package com.infotech;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -13,10 +14,19 @@ public class NashornJavaScriptEngineClient56 {
         ScriptEngine engine = scriptEngineManager.getEngineByName("Nashorn");
         try {
             engine.eval("print('Hello World!')");
+            System.out.println("+++++++++++++++++++++++++++++++++++++++");
             engine.eval(new FileReader("src\\demo.js"));
+            System.out.println("+++++++++++++++++++++++++++++++++++++++");
+            engine.eval(new FileReader("src/demoFunc.js"));
+            Invocable invocable = (Invocable) engine;
+            Object result = invocable.invokeFunction("func", "Mahdiye Nikraftar");
+            System.out.println("+++++++++++++++++++++++++++++++++++++++");
+            System.out.println(result);
         } catch (ScriptException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
 
