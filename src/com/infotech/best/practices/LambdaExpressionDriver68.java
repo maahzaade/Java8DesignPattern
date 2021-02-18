6b8b15725937b317d41fe110f42ec8d965882bd4
mahdiye.nikraftar <mahdiye.nikraftar@cgi.com>
2021-02-18 18:59:38 -0500

    Best Practices UTs are added

---
+++ src/com/infotech/best/practices/LambdaExpressionDriver68.java
@@ -0,0 +1,27 @@
+package com.infotech.best.practices;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class LambdaExpressionDriver68 {
+
+    public static void main(String[] args) {
+
+        BiFunction<String, String, String> biFunction = (s1, s2) -> getFullName(s1, s2);
+        String fullName = biFunction.apply("Mahdiye", "Nikraftar");
+        System.out.println(fullName);
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");
+        Function<String, String> function = s->"Hi "+s;
+        String apply = function.apply("Adrian");
+        System.out.println(apply);
+    }
+
+    private static String getFullName(String s1, String s2) {
+        String firstName = s1.toUpperCase();
+        String lastName = s2.toUpperCase();
+
+        String result = firstName + "\t" + lastName;
+        return result;
+    }
+}
