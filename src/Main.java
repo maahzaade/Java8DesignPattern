34939c9455ab92ee2e575471d0c0fba517309a85
mahdiye.nikraftar <mahdiye.nikraftar@cgi.com>
2021-03-22 10:55:00 -0400

    enhanced loop UT

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        List<String> names = new ArrayList<String>();
+
+        names.add("Mahdiye");
+        names.add("Harish");
+        names.add("Sandeep");
+        names.add("Uma");
+
+
+        // what is names = null; we have to have a validation
+        for(String item: names){
+            System.out.println(item);
+        }
+
+
+
     }
 }
