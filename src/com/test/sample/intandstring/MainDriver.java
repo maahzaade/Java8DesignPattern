1f7b7b4b084dbd91071a28cf2d03b570191ebaca
maahzaade <mahdiye.nikraftar@gmail.com>
2021-03-25 18:53:39 -0400

    new completed method added

--- src/com/test/sample/intandstring/MainDriver.java
+++ src/com/test/sample/intandstring/MainDriver.java
@@ -3,8 +3,9 @@ package com.test.sample.intandstring;
 public class MainDriver {
 
     public static void main(String[] args) {
-        StringToint("23");
+//        StringToint("23");
         IntToString(23);
+        myAtoi("23");
 
         System.out.println(Integer.parseInt("34"));
         System.out.println(String.valueOf(34));
@@ -27,6 +28,33 @@ public class MainDriver {
         return reVal;
 
     }
+
+    public static int myAtoi(String str) {
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+        if (str.length() == 0) return 0;
+
+        //Discard whitespaces in the beginning
+        while (i < str.length() && str.charAt(i) == ' ')
+            i++;
+
+        // Check if optional sign if it exists
+        if (i < str.length() && (str.charAt(i) == '+' || str.charAt(i) == '-'))
+            sign = (str.charAt(i++) == '-') ? -1 : 1;
+
+        // Build the result and check for overflow/underflow condition
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            if (result > Integer.MAX_VALUE / 10 ||
+                    (result == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + (str.charAt(i++) - '0');
+        }
+        return result * sign;
+
+    }
+
 
     public static String IntToString(int number) {
         int StringConvet = 48;
