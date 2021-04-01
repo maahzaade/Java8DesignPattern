326767ef29d99877bb30d75a4fd6bd6ee4e439bf
maahzaade <mahdiye.nikraftar@gmail.com>
2021-04-01 12:26:18 -0400

    New module created to start developing a Restfull API with SpringBoot

---
+++ restfullAPIDemo/src/main/java/com/springboot/example/restfullAPIDemo/RestfullApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.example.restfullAPIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullApiDemoApplication.class, args);
+	}
+
+}
---
+++ restfullAPIDemo/src/test/java/com/springboot/example/restfullAPIDemo/RestfullApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.example.restfullAPIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
