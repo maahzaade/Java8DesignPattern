ab43ed7917a1ea95b1ab811ba6c164aab566c309
maahzaade <mahdiye.nikraftar@gmail.com>
2021-04-24 20:41:04 -0400

    Handling Multiple Filters in Spring Boot

--- springCMS/src/main/java/com/rest/full/api/springcms/filter/MyFilter_2.java
+++ springCMS/src/main/java/com/rest/full/api/springcms/filter/MyFilter_2.java
@@ -9,11 +9,11 @@ import javax.servlet.*;
 import java.io.IOException;
 
 @Component
-@Order(1)
-public class MyFilter_1 implements Filter {
+@Order(3)
+public class MyFilter_2 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("Filter 1 is called...");
+        System.out.println("Filter 2 is called...");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
